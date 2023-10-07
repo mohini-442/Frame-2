354d78f9b7e2ad009a6a2c0788d917c68d32d799
mohini-442 <mohinikarwasra598@gmail.com>
2023-10-06 21:44:42 -1200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// back to top-------
+window.addEventListener('scroll', function () {
+    let backtotop = document.getElementById("backtotop")
+    if (window.scrollY > 300) {
+        backtotop.style.display = 'block'
+    }
+    else {
+        backtotop.style.display = 'none'
+    }
+})
